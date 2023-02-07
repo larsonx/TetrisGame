90da9ca8c6996b945925685b3c3983c55d90ee36
larsonx <97100495@st.deltion.nl>
2023-02-07 12:21:40 +0100

    update

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  /*Creert 200 divs in HTML.*/
+  for (x = 0; x < 200; x++) {
+    let grid = document.createElement("div");
+    grid.className = "grid";
+    document.getElementById("board")
+    .appendChild(grid);
+  }
+const grid = document.querySelector('.grid')
+let vierkanten = Array.from(document.querySelectorAll('.grid div'))
+const ScoreDisplay = getElementById('#score')
+const StartBtn = getElementById('#start-button')
+const width = 10;
 
+
+
+
+});
