24fb38a636a3e14c6775e5ac8fbf6a531b5853e0
larsonx <97100495@st.deltion.nl>
2023-02-08 15:42:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,35 +3,33 @@ document.addEventListener("DOMContentLoaded", () => {
   for (x = 0; x < 200; x++) {
     let grid = document.createElement("div");
     grid.className = "grid";
-    document.getElementById("board")
-    .appendChild(grid);
+    document.getElementById("board").appendChild(grid);
   }
   /*Maakt een Array van de 200 div elementen */
-const grid = document.querySelector('.grid')
-let vierkanten = Array.from(document.querySelectorAll('.grid div'))
-const ScoreDisplay = document.querySelector('#score')
-const StartBtn = document.querySelector('#start-button')
-const width = 10;
+  const grid = document.querySelector(".grid");
+  let vierkanten = Array.from(document.querySelectorAll(".grid div"));
+  const ScoreDisplay = document.querySelector("#score");
+  const StartBtn = document.querySelector("#start-button");
+  const width = 10;
 
-//Tetromino//
-const lTetromino = [
-  [1, width+1, width*2, 2],
-  [width, width+1, width+2, width*2+2],
-  [1, width+1, width*2+1, width*2],
-  [width, width*2, width*2+1, width*2+2]
-]
-const zTetromino = [
-[0, width,width+1,width*2+1],
-[width+1, width+2,width*2,width*2+1],
-[0,width,width+1,width*2+1],
-[width+1, width+2,width*2,width*2+1]
-
-]
-const oTetromino = [
-[0,1,width,width+1]
-
-]
-
-
+  //Tetromino//
+  const lTetromino = [
+    [1, width + 1, width * 2, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ];
 
 });
