62a6f530c43f99dc3802bae0cf823dbb188ee256
larsonx <97100495@st.deltion.nl>
2023-02-13 11:42:21 +0100

    l

--- app.js
+++ app.js
@@ -5,44 +5,67 @@ document.addEventListener("DOMContentLoaded", () => {
     grid.className = "grid";
     document.getElementById("board").appendChild(grid);
   }
-  /*Maakt een Array van de 200 div elementen */
+
   const grid = document.querySelector(".grid");
-  let vierkanten = Array.from(document.querySelectorAll(".grid div"));
+  let squares = Array.from(document.querySelectorAll(".grid div"));
   const ScoreDisplay = document.querySelector("#score");
   const StartBtn = document.querySelector("#start-button");
   const width = 10;
 
-  //Tetromino//
+  //Tetromino + Posities//
   const lTetromino = [
-    [1, width + 1, width * 2, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2],
-  ];
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
   const zTetromino = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
   ];
+
   const tTetromino = [
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1],
   ];
+
   const oTetromino = [
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
   ];
+
   const iTetromino = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
   ];
-const theTetrominoes = [lTetromino, zTetromino, tTetromino, iTetromino]
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
 
+  let currentPosition = 4;
+  let currentRotation = 0
+
+  //Selecteert een Tetromino en de standaard positie
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][0];
+
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add("tetromino");
+    });
+  }
+  draw();
 });
